d213f8f71fb68df5a7016070fa0f793013ebb141
LAPTOP-BGOBC1RM\jmwol <jwolak73@gmail.com>
2022-07-24 15:05:54 -0400

    create a java file called JavaFile.java

---
+++ JavaFile.java
@@ -0,0 +1,11 @@
+package BranchExercise;
+
+/**
+ * JavaFile
+ */
+public class JavaFile {
+
+    public static void main(String[] args) {
+        System.out.println("This is a javaFile");
+    }
+}
